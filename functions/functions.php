800da97c05b10c560952e11cd96b4991f0722a3a
aggviolinist <mulandimaei76@gmail.com>
2022-11-19 02:39:35 -0500

    "LPG SYSTEM"

--- functions/functions.php
+++ functions/functions.php
@@ -41,5 +41,33 @@ function getWeight()
             
                         
 }
+
+function display(){
+    global $connection; //connection
+
+
+    $display ="select * from products order by RAND() LIMIT 0,6"; //selecting 6 products to randomly show on the home page
+
+    $display_run = mysqli_query($connection,$display);
+    
+    while($fetch_row=mysqli_fetch_array($display_run)){ //while loop to fetch data from the DB by fetching array of display run
+
+        $display_id = $fetch_row['gas_id'];//id field
+        $display_name = $fetch_row['gas_name'];//name field
+        $display_comapny = $fetch_row['gas_company'];//company field
+        $display_weight = $fetch_row['gas_weight'];//weight field
+        $display_price = $fetch_row['gas_price'];//price field
+        $display_description = $fetch_row['gas_description'];//description field
+        $display_image = $fetch_row['gas_image'];//image field
+        $display_keywords = $fetch_row['gas_keywords'];//keywords field
+
+
+        
+    }
+
+
+}
+
+
 ?>
 
