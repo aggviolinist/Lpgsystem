a1b43aa122418d1fa545534d2c336f5ba9c918d4
aggviolinist <mulandimaei76@gmail.com>
2022-11-30 05:26:11 -0500

    LPG system

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
  <?php
  include("functions/functions.php"); //includes specific folders and files into html
 
- ini_set("display_errors","1");
- ini_set("display_startup_errors","1");
- error_reporting(E_ALL);
+ //ini_set("display_errors","1");
+ //ini_set("display_startup_errors","1");
+ //error_reporting(E_ALL);
  ?>
     <head>
     <title>BOOK A CYLINDER</title>
