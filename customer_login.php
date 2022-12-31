eaa4f0ec31cd2cc8aa8f491e3b6a0da71a1d97c6
aggviolinist <mulandimaei76@gmail.com>
2022-12-30 23:24:20 -0500

    LPG system

--- customer_login.php
+++ customer_login.php
@@ -3,20 +3,20 @@
     <form method="post" action="">
         <table width="500" align="center" bgcolor="#ADD8E6">
             <tr align="center">
-                <td colspan="4"><h2>Register and login to make your booking!!!!!</h2></td>
+                <td colspan="10"><h2>Register and login to make your booking!!!!</h2></td>
             </tr>
-            <tr>
-                <td align="right"><b>Email:</b></td>
-                <td><input type="text" name="email" placeholder="enter your email" /></td>
+            <tr colspan="10">
+                <td align="right"><br><b>Email:</b></td>
+                <td><br><input type="text" name="email" placeholder="enter your email" /></td>
             </tr>
-            <tr>
-                <td align="right"><b>Password:</b></td>
-                <td><input type="password" name="password" placeholder="enter your password"/></td>
+            <tr colspan="100">
+                <td align="right"><br><b>Password:</b></td>
+                <td><br><input type="password" name="password" placeholder="enter your password"/></td>
             </tr>
             <br>
             <tr>
                 <br>
-                <td align="right" colspan="1"><input type="submit" name="login" value="login"/></td>
+                <td align="right" colspan="1"><br><input type="submit" name="login" value="login"/></td>
             </tr>
 
         </table>
